a6847430552a0f3fd7d0950936fa2939b9d2a201
Faris <fariswy2@gmail.com>
2026-04-21 09:13:10 +0000

    Keep all configured teams in league projections

--- premier-league-predictor/app.py
+++ premier-league-predictor/app.py
@@ -426,7 +426,7 @@ def get_cached_data(league):
         return None, None
     
     teams = LEAGUE_DATA[league]["teams"]
-    available_teams = [t for t in teams if t in df['HomeTeam'].values or t in df['AwayTeam'].values]
+    available_teams = list(teams)
     
     model = EnhancedPoissonModel()
     model.fit(df, available_teams)
